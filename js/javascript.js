1b47aa4a71abe375aac3eb1e2d1bfdbe4bf57197
Joan Cruz <joancruzcortes@gmail.com>
2025-05-08 23:52:19 -0500

    javascript updated

--- js/javascript.js
+++ js/javascript.js
@@ -1,2 +1,32 @@
-'use strict'; 
+'use strict';
 
+document.addEventListener('DOMContentLoaded', function() {
+    const enviarBtn = document.querySelector('#enviarDatos');
+    const nombreInput = document.querySelector('#floatingInputNombre');
+    const edadInput = document.querySelector('#floatingInputEdad');
+    const numeroTelefonoInput = document.querySelector('#floatingInputNumero');
+    const linkInstaInput = document.querySelector('#floatingInputInstagram');
+    const correoInput = document.querySelector('#floatingInputGrid');
+    const hinchaSelect = document.querySelector('#floatingSelectGrid');
+    const tuMensajeTextarea = document.querySelector('#floatingTextarea');
+
+    enviarBtn.addEventListener('click', function() {
+        const nombreValue = nombreInput.value;
+        const edadValue = edadInput.value;
+        const numeroTelefonoValue = numeroTelefonoInput.value;
+        const linkInstaValue = linkInstaInput.value;
+        const correoValue = correoInput.value;
+        const hinchaValue = hinchaSelect.value;
+        const tuMensajeValue = tuMensajeTextarea.value;
+
+        const datosRegistro = {
+            nombre: nombreValue,
+            edad: edadValue,
+            numeroTelefono: numeroTelefonoValue,
+            linkInsta: linkInstaValue,
+            correo: correoValue,
+            hincha: hinchaValue,
+            tuMensaje: tuMensajeValue
+        };
+    });
+});
